eb5b2c022956ac6fbbbf78f46aa8729ec0b173ab
©StalingradTeam <70740073+StalingradTeam@users.noreply.github.com>
2021-03-06 20:32:54 +0000

    Add files via upload

---
+++ tasks/signals.py
@@ -0,0 +1,44 @@
+from django.db.models.signals import m2m_changed
+from django.db.models import signals
+from django.dispatch import receiver
+from tasks.models import TodoItem, Category
+from collections import Counter
+
+
+@receiver(m2m_changed, sender=TodoItem.category.through)
+def task_cats_added(sender, instance, action, model, **kwargs):
+    if action != "post_add":
+        return
+
+    for cat in instance.category.all():
+        slug = cat.slug
+
+        new_count = 0
+        for task in TodoItem.objects.all():
+            new_count += task.category.filter(slug=slug).count()
+
+        Category.objects.filter(slug=slug).update(todos_count=new_count)
+
+
+@receiver(m2m_changed, sender=TodoItem.category.through)
+def task_cats_removed(sender, instance, action, model, **kwargs):
+    if action != "post_remove":
+        return
+
+    cat_counter = Counter()
+    for t in TodoItem.objects.all():
+        for cat in t.category.all():
+            cat_counter[cat.slug] += 1
+
+    for slug, new_count in cat_counter.items():
+        Category.objects.filter(slug=slug).update(todos_count=new_count)
+
+@receiver(signals.post_save, sender=TodoItem)
+def todo_created(sender, instance, created, **kwargs):
+    pri_counter = Counter()
+    for t in TodoItem.objects.all():
+        pri = t.priority
+        pri_counter[pri] += 1
+
+    for pri, new_count in pri_counter.items():
+        TodoItem.objects.filter(priority=pri).update(pri_count=new_count)
